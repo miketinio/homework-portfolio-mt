c30a81e9c4cd785e9d8220edc814733f8d7f390f
Mike <miketinio@gmail.com>
2018-05-17 14:24:39 -0700

    finished-assignment

--- Unit 3/js/index.js
+++ Unit 3/js/index.js
@@ -1,3 +1,22 @@
+$(document).ready(function() {
+    $('.hide').hide();
+    $('.learnmore').click(function(){
+    	event.preventDefault();
+    	$('#learnmoretext').slideDown();
+    	$('.learnmore').hide();
+  	});
 
+    $('.readmore').click(function(){
+    	event.preventDefault();
+    	$('#show-this-on-click').slideDown();
+    	$('.readmore').hide();
+    	$('.readless').show();
+    });
 
-
+	$('.readless').click(function(){
+    	event.preventDefault();
+    	$('#show-this-on-click').slideUp();
+    	$('.readmore').show();
+    	$('.readless').hide();
+    });    
+  });
