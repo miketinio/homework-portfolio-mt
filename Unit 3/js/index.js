417659790c65ea477798581480a90e5c2adcfb04
Mike <miketinio@gmail.com>
2018-05-17 12:16:14 -0700

    learnmore changes

--- Unit 3/js/index.js
+++ Unit 3/js/index.js
@@ -1,13 +1,3 @@
-function () {
-	function showLearnMore() {
-	//event.preventDefault();
-	console.log('i\'ve been clicked');
-	$('#learnmoretext').slidetoggle();
-	}
-
-	$('.learnmore').click(showLearnMore);
-
-}
 
 
 
